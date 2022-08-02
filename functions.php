76bf2944a98f6abb756d667eab2461f8cccad33f
tejas <liktejas@gmail.com>
2022-08-02 09:29:16 +0530

    Add support for google fonts

--- functions.php
+++ functions.php
@@ -74,3 +74,19 @@ add_action( 'wp_enqueue_scripts', 'tailwindfse_styles' );
 require get_template_directory() . '/inc/block-patterns.php';
 
 add_action( 'customize_register', '__return_true' );
+
+/**
+ * Add Google Fonts.
+ *
+ * @since tailwindfse 1.0
+ *
+ * @return void
+ */
+function tailwindfse_add_google_fonts() {
+	?>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+	<?php
+}
+
+add_action( 'wp_head', 'tailwindfse_add_google_fonts' );
+
