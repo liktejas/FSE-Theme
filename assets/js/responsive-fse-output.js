aa73824e2fc88cf427be0007ea338d96e7a6b47d
tejas <liktejas@gmail.com>
2022-08-30 17:40:38 +0530

    Change layout and js logic of templates

--- assets/js/responsive-fse-output.js
+++ assets/js/responsive-fse-output.js
@@ -1,9 +1,16 @@
+if ( !jQuery('#responsive-fse-pagination').children().length > 0 ) {
+    jQuery('#responsive-fse-pagination').hide()
+}
 if ( !jQuery('#responsive-fse-search-pagination').children().length > 0 ) {
     jQuery('#responsive-fse-search-pagination').hide()
 }
-if ( !jQuery('#responsive-fse-single-post-comments-query-loop').children().length > 0 ) {
+if ( !jQuery('.single.single-post #responsive-fse-single-post-comments .wp-block-comments-query-loop').children().length > 0 ) {
     jQuery('#responsive-fse-single-post-comments').hide()
     jQuery('#responsive-fse-single-post-content').css('margin-bottom', '20px')
+}
+if ( !jQuery('.single.single-post #responsive-fse-single-post-pagination .wp-block-post-navigation-link').children().length > 0 ) {
+    jQuery('#responsive-fse-single-post-pagination').hide()
+    jQuery('#responsive-fse-page-content').css('margin-bottom', '40px')
 }
 if ( !jQuery('.page .wp-block-comments-query-loop').children().length > 0 ) {
     jQuery('#responsive-fse-page-comments').hide()
